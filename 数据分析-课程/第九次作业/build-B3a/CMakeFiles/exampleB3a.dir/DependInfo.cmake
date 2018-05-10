2bf9ac0ff1b7912bb42c7390db38b5575f47c56a
gcxist <544768220@qq.com>
2018-05-10 23:38:09 +0800

    new

---
+++ 数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/exampleB3a.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/exampleB3a.cc.o"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/src/B3DetectorConstruction.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/src/B3DetectorConstruction.cc.o"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/src/B3PhysicsList.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/src/B3PhysicsList.cc.o"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/src/B3PrimaryGeneratorAction.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/src/B3PrimaryGeneratorAction.cc.o"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/src/B3StackingAction.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/src/B3StackingAction.cc.o"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/src/B3aActionInitialization.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/src/B3aActionInitialization.cc.o"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/src/B3aEventAction.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/src/B3aEventAction.cc.o"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/src/B3aRunAction.cc" "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/build-B3a/CMakeFiles/exampleB3a.dir/src/B3aRunAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/projects/soft/ext/geant4.10.00.p02-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "/home/gucxist/Desktop/data-analysis/数据分析-课程/第九次作业/B3a/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
