5919491c9a30d05baeb2a4575947ffe658c0584b
Gu Chenxi <guchenxi@Gus-MacBook-Pro.local>
2018-05-10 21:25:20 +0800

    new

--- 数据分析-课程/第九次作业/pro9_1.C
+++ 数据分析-课程/第九次作业/pro9_1.C
@@ -1,18 +1,18 @@
 using namespace std;
-int pro8_2(){
+int pro9_1(){
    gStyle->SetOptStat(kFALSE);
 TH1F* mean = new TH1F("mean","mean",30,-0.5,0.5);
 TH1F* variant = new TH1F("variant","variant",30,0.5,2); 
-TH1F* chindf = new TH1F("chindf","chindf",30,0,4); 
+TH1F* likelihood = new TH1F("likelihood","likelihood",30,0,30); 
 
 TH1F* variant1 = new TH1F("variant1","variant1",30,0.5,2);
-TH1F* chindf1 = new TH1F("chindf1","chindf1",30,0,4);
+TH1F* likelihood1 = new TH1F("likelihood1","likelihood1",30,0,30);
 
 TH1F* variant2 = new TH1F("variant2","variant2",30,0.5,2);
-TH1F* chindf2 = new TH1F("chindf2","chindf2",30,0,4);
+TH1F* likelihood2 = new TH1F("likelihood2","likelihood2",30,0,30);
 
 TH1F* variant3 = new TH1F("variant3","variant3",30,0.5,2);
-TH1F* chindf3 = new TH1F("chindf3","chindf3",30,0,4);
+TH1F* likelihood3 = new TH1F("likelihood3","likelihood3",30,0,30);
 
 TF1* fcn = new TF1("fcn","gaus",-5,5);
 TF1* fcn1 = new TF1("fcn1","[0] * TMath::Gaus(x,0.05,[1])",-5,5);
@@ -28,30 +28,34 @@ double par[2]={1,1};
 				gsd->Fill(gRandom->Gaus(0,1));
 			}
 		fcn->SetParameters(10,0,1);
-		gsd->Fit(fcn,"R");
-		chindf->Fill(fcn->GetChisquare()/fcn->GetNDF());
+		gsd->Fit(fcn,"RLS");
+		TFitResultPtr r = gsd->Fit(fcn, "RLS");
+		likelihood->Fill(r->MinFcnValue());
 		Double_t mypara[3];
 		fcn->GetParameters(&mypara[0]);
 		mean->Fill(mypara[1]);
 		variant->Fill(mypara[2]);
-		
+
 		fcn1->SetParameters(1,1);
-		gsd->Fit(fcn1,"R");
-		chindf1->Fill(fcn1->GetChisquare()/fcn1->GetNDF());
+		gsd->Fit(fcn1,"RLS");
+		TFitResultPtr r1 = gsd->Fit(fcn1, "RLS");
+        likelihood1->Fill(r1->MinFcnValue());
 		Double_t mypara1[2];
 		fcn1->GetParameters(&mypara1[0]);
 		variant1->Fill(mypara1[1]);
 
 		fcn2->SetParameters(1,1);
-        gsd->Fit(fcn2,"R");
-        chindf2->Fill(fcn2->GetChisquare()/fcn2->GetNDF());
+        gsd->Fit(fcn2,"RLS");
+        TFitResultPtr r2 = gsd->Fit(fcn2, "RLS");
+        likelihood2->Fill(r2->MinFcnValue());
         Double_t mypara2[2];
         fcn2->GetParameters(&mypara2[0]);
         variant2->Fill(mypara2[1]);
 
 		fcn3->SetParameters(1,1);
         gsd->Fit(fcn3,"R");
-        chindf3->Fill(fcn3->GetChisquare()/fcn3->GetNDF());
+		TFitResultPtr r3 = gsd->Fit(fcn3, "RLS");
+        likelihood3->Fill(r3->MinFcnValue());
         Double_t mypara3[2];
         fcn3->GetParameters(&mypara3[0]);
         variant3->Fill(mypara3[1]);
@@ -77,20 +81,20 @@ double par[2]={1,1};
     TCanvas * c2 = new TCanvas("c2", "c2", 1000, 1000);
 
     TLegend lege1(0.7,0.7,0.9,0.9);
-    lege1.AddEntry(chindf,"all free","l");
-    lege1.AddEntry(chindf1,"mu=0.05","l");
-	lege1.AddEntry(chindf2,"mu=0.2","l");
-	lege1.AddEntry(chindf3,"mu=0.5","l");
-    chindf->SetTitle("chi2/ndf");
-    chindf1->SetLineColor(kRed);
-	chindf2->SetLineColor(kBlue);
-	chindf3->SetLineColor(kGreen);
-    chindf->Draw();
-    chindf1->Draw("same");
-	chindf2->Draw("same");
-	chindf3->Draw("same");
+    lege1.AddEntry(likelihood,"all free","l");
+    lege1.AddEntry(likelihood1,"mu=0.05","l");
+	lege1.AddEntry(likelihood2,"mu=0.2","l");
+	lege1.AddEntry(likelihood3,"mu=0.5","l");
+    likelihood->SetTitle("likelihood");
+    likelihood1->SetLineColor(kRed);
+	likelihood2->SetLineColor(kBlue);
+	likelihood3->SetLineColor(kGreen);
+    likelihood->Draw();
+    likelihood1->Draw("same");
+	likelihood2->Draw("same");
+	likelihood3->Draw("same");
     lege1.Draw();
-    c2->SaveAs("chi2ndf.pdf");
+    c2->SaveAs("likelihood.pdf");
 
 
 
