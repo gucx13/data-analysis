302ef3116714603a9eca89d6e8a4f99f9575b407
Gu Chenxi <guchenxi@Gus-MacBook-Pro.local>
2018-05-24 00:16:37 +0800

    new

--- 粒子物理/chapter-5/Create_Feynman_Diagrams/create_diagrams.sh
+++ 粒子物理/chapter-5/Create_Feynman_Diagrams/create_diagrams.sh
@@ -1,11 +1,11 @@
 #latex BsJpsiPhi_penguin_example.tex
-latex e_mu_Fey.tex
+latex e_mu_Fey_b.tex
 rm *.aux
 rm *.log
 #dvips BsJpsiPhi_penguin_example.dvi
-dvips e_mu_Fey.dvi
+dvips e_mu_Fey_b.dvi
 #ps2pdf BsJpsiPhi_penguin_example.ps
-pstopdf e_mu_Fey.ps
+pstopdf e_mu_Fey_b.ps
 # Remove if want to keep dvi format:
 rm *.dvi
-open e_mu_Fey.pdf
+open e_mu_Fey_b.pdf
